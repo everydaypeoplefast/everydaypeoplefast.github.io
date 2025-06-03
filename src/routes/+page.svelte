7243bea4f65a21fcd022c44fa3088cf16b65dd75
everydaypeoplefast <info@everydaypeoplefast.com>
2025-06-03 10:41:21 -0400

    abunch of changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<ImageWithContent
+  heading="We aim to move beyond our shock and grief to address the ongoing threat of white supremacist violence in a more direct way."
+  subheading="About Us"
+  content="As Everyday People fed up and alinve in this time and place, we have experienced and born witness to too many atrocities, and while we are shocked, we will not let our shock be the only result of what we have witnessed and experienced. We will also act and organize."
+  cta_title="Learn More"
+  cta_link="/your-link"
+  image_alt="Descriptive alt text"
+  image_first={true}
+  color_scheme="light"
+/>
